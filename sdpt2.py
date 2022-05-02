c4f83cecbed7d50d0e59170288f68d8d48799c5c
LEIGH <leighest2021@gmail.com>
2022-05-02 15:33:40 +0800

    SDPT Part 11

--- sdpt2.py
+++ sdpt2.py
@@ -612,13 +612,90 @@
 #     else:
 #         print("Wrong input.")
 #
+#
+# # challenge - square a number using functions w/ parameters
+#
+#
+# def square(n):
+#     return n * n
+#
+#
+# num = int(input("Input: "))
+# print(f"Output: {square(num)}")
+#
+#
+# print()
+# # PART 11: ARGUMENTS, ARBITRARY ARGUMENTS, KEYWORD ARGUMENTS
+#
+#
+# def say_hello(name):
+#     print("Hello,", name)
+#
+#
+# i = 1
+# while i > 0:
+#     say_hello(input("Enter a name: "))  # using function to input
+#
+#
+# # arbitrary arguments - asterisk args
+# # can't have two in a function, it is neverending
+#
+#
+# def say_hi(*names):  # it converts to tuples, unspecified size
+#     for name in names:
+#         print("Hi,", name)
+#
+#
+# say_hi("Leigh", "Inna", "Jamolin",)
+#
+# # keyword arguments(kwargs) - an alternative way for sending arguments inside a function by specifying
+# # the parameter name in no certain order. Often used in combination with arbitrary args or if you don't know
+# # the order of the arguments in the function.
+#
+#
+# def jaja(fname, lname):
+#     print(fname + " " + lname)
+#
+#
+# jaja(lname="Jamolin", fname="Leigh")  # using kwargs
+#
+#
+# def jamolin_fam(*fnames, lname):
+#     for fname in fnames:
+#         print(fname + " " + lname)
+#
+#
+# jamolin_fam("Lionel", "Gina", "Leigh", "Janna", "Mira", lname="Jamolin")
+#
+# # arbitrary keyword arguments(**kwargs)
+# # used when you are uncertain on what parameter name and how many param you want to pass
+#
+#
+# def students_info(**student):
+#     print(student["fname"] + " " + student["lname"])
+#     print(f"Age:", student["age"])
+#     print(f"Course: " + student["course"])
+#
+#
+# # keywords are always strings
+# fname = input("Enter first name: ")
+# lname = input("Enter last name: ")
+# age = int(input("Enter age: "))
+# course = input("Enter course: ")
+#
+# print("Student #1")
+# students_info(fname=fname, lname=lname, course=course, age=age)  # no certain order
+#
 
-# challenge - square a number using functions w/ parameters
+# challenge - summation program
 
 
-def square(n):
-    return n * n
+def summation(*num):
+    x = 0
+    for n in num:
+        x += n
+    return x
 
 
-num = int(input("Input: "))
-print(f"Output: {square(num)}")
+print(summation(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
