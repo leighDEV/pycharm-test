610da79462f440a70e4d6079f1da1f3540e51e55
LEIGH <leighest2021@gmail.com>
2022-05-02 22:21:02 +0800

    SDPT Part 14
    constructors

--- sdpt-oop.py
+++ sdpt-oop.py
@@ -1,40 +1,55 @@
-# PART 13: OOP | CLASSES AND OBJECTS
+# # PART 13: OOP | CLASSES AND OBJECTS
+#
+# class Enemy:
+#     name = "Name"  # default value
+#     hp = 100
+#     mp = 50
+#     atk = 12
+#     lvl = 1
+#
+#
+# enemy_one = Enemy()
+# enemy_one.name = "Leigh"
+# enemy_one.hp = 99
+# enemy_one.mp = 50
+# enemy_one.atk = 8
+# enemy_one.lvl = 99
+#
+# enemy_two = Enemy()
+# enemy_two.name = "Inna"
+#
+# print(enemy_one.name)
+# print(enemy_two.name)
+#
+#
+# class Product:
+#     brand = "Brand"
+#     id = 1000
+#     qty = 0
+#
+#
+# product_one = Product()
+# product_one.brand = "Tesla"
+# product_one.id = 999
+# product_one.qty = 9
+#
+# print(product_one.brand)
+# print(product_one.id)
+# print(product_one.qty)
+#
+# product_two = Product()
+#
 
-class Enemy:
-    name = "Name"  # default value
-    hp = 100
-    mp = 50
-    atk = 12
-    lvl = 1
+# PART 14: CONSTRUCTORS
+
+class Student:  # everytime we call this, the constructors also run
+
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+        print(f"Name: {name}\nAge: {age}\nCourse: {course}")
 
 
-enemy_one = Enemy()
-enemy_one.name = "Leigh"
-enemy_one.hp = 99
-enemy_one.mp = 50
-enemy_one.atk = 8
-enemy_one.lvl = 99
-
-enemy_two = Enemy()
-enemy_two.name = "Inna"
-
-print(enemy_one.name)
-print(enemy_two.name)
-
-
-class Product:
-    brand = "Brand"
-    id = 1000
-    qty = 0
-
-
-product_one = Product()
-product_one.brand = "Tesla"
-product_one.id = 999
-product_one.qty = 9
-
-print(product_one.brand)
-print(product_one.id)
-print(product_one.qty)
-
-product_two = Product()
+student_one = Student("Leigh", 19, "BSCS")
+print(student_one.age)
