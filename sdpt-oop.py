2a322dc420a4ece24ddc1a9d449520e2a7fa95cd
LEIGH <leighest2021@gmail.com>
2022-05-03 10:28:00 +0800

    SDPT Part 15
    methods

--- sdpt-oop.py
+++ sdpt-oop.py
@@ -39,17 +39,69 @@
 #
 # product_two = Product()
 #
+#
+# # PART 14: CONSTRUCTORS
+#
+# class Student:  # everytime we call this, the constructors also run
+#
+#     def __init__(self, name, age, course):
+#         self.name = name
+#         self.age = age
+#         self.course = course
+#
+#     def introduce(self):
+#         print(f"Name: {self.name}\nAge: {self.age}\nCourse: {self.course}")
+#
+#
+# student_one = Student("Leigh", 19, "BSCS")
+# student_one.introduce()  # printing already
+#
+#
+# # PART 15: OBJECT FUNCTIONS
+# # also called methods (functions inside a class) - represent the object's purpose
+# # constructors are also an object functions
+#
+# class Animal:
+#     def __init__(self, type, sound):
+#         self.type = type
+#         self.sound = sound
+#
+#     def speak(self):  # methods always have self parameter at first
+#         print(f"{self.type}: {self.sound}")
+#
+#
+# animal_dog = Animal("Dog", "Woof")
+# animal_dog.speak()
+# animal_cat = Animal("Cat", "Meow")
+# animal_cat.speak()
+#
 
-# PART 14: CONSTRUCTORS
+# challenge - user in a social media
 
-class Student:  # everytime we call this, the constructors also run
+class User:
+    def __init__(self, first_name, last_name, likes_count, friends_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.likes_count = likes_count
+        self.friends_name = friends_name
+        print(f"User created. Welcome, {self.first_name}!")
 
-    def __init__(self, name, age, course):
-        self.name = name
-        self.age = age
-        self.course = course
-        print(f"Name: {name}\nAge: {age}\nCourse: {course}")
+    def introduce(self):
+        print(f"Hi, I am  {self.first_name} {self.last_name}!")
+
+    def full_profile(self):
+        print(f"Full name: {self.first_name} {self.last_name}")
+        print(f"Likes    : {str(self.likes_count)}")
+        print("Friends  : ")
+        for friend in self.friends_name:  # this is a list
+            print(" -" + friend)
 
 
-student_one = Student("Leigh", 19, "BSCS")
-print(student_one.age)
+user_one = User("Leigh", "Jamolin", 10, ["Harry", "Ron", "Hermione"])
+user_one.introduce()
+user_one.full_profile()
+
+print()
+user_two = User("Hermione", "Granger", 99, ["Harry", "Ron", "Ginny"])
+user_two.introduce()
+user_two.full_profile()
